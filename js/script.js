9df55729709c321b605d33895ed31145e31730c2
pascArr <pascual_arr@hotmail.com>
2017-02-12 14:59:17 -0800

    final review

--- js/script.js
+++ js/script.js
@@ -87,39 +87,32 @@ $ajaxUtils.sendGetRequest(
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
+
+
 // Builds HTML for the home page based on categories array
 // returned from the server.
 function buildAndShowHomeHTML (categories) {
 
-  switchMenuToActive();
+  // Load home snippet page
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      var chosenCategoryShortName = chooseRandomCategory (categories);
-      var homeHtml = "{{" + chosenCategoryShortName.short_name + "}}";
+      switchMenuToActive();
+      var chosenCategoryShortName = chooseRandomCategory(categories);
+      var str = "'" + String(chosenCategoryShortName.short_name) + "'";     
       var homeHtmlToInsertIntoMainPage = 
-        insertProperty(homeHtmlUrl, {{randomCategoryShortName}}, homeHtml );
-          insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
-        },
-        false);
-        }
-  }
-
-
-
-
-
-
-
+        insertProperty(homeHtml, 'randomCategoryShortName', str);      
+      insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+    },
+    false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+}
+     
 
       
-
 
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
-
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -131,14 +124,15 @@ function buildAndShowHomeHTML (categories) {
       // $dc.loadMenuItems('L')
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
-      //
-      // var homeHtmlToInsertIntoMainPage = ....
+      
+      
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+
 
 
 
